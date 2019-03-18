f6802a0bd9a9b727a9a1cc2b3539d3fde7585dca
S B <sb@ss-mb-pro.home>
2019-03-18 18:08:36 +0000

    Increased the size of title

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-             <code>Welcome to Kodflix</code> 
+             <h1>Welcome to Kodflix!!</h1> 
           </p>
           <a
             className="App-link"
