b467b54c7fc8395a71e4b0410a0c5011b8c5e55a
S B <sb@DESKTOP-771PQMS.localdomain>
2019-04-02 17:38:54 +0100

    Random spaces Test

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className = "App">
         <b /><b /><b />
         <div className="container">
           <Stack name='Mulan' logo={Mulan} />
