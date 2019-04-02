d3d114b5be140fb330c114857812df8a24660203
S B <sb@DESKTOP-771PQMS.localdomain>
2019-04-02 11:10:26 +0100

    Replace TV show titles with cover_2

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
 import React, { Component } from 'react';
-/*import JScriptLogo from './Images/JSlogo.jpeg';*/
+import Mulan from './Images/Mulan.jpeg';
+import PatchAdams from './Images/PatchAdams.jpeg';
+import Elf from './Images/Elf.jpeg';
+import ShawshankRedemption from './Images/Shawshank.jpeg';
+import StElmos from './Images/StElmos.jpeg';
+import BfastClub from './Images/BfastClub.jpeg';
 import './App.css';
 
 class App extends Component {
   render() {
       return (
           <div className="App">
-            <br /><br /><br />
-            <div className="container"> 
-              <div className='item'><h1> Mulan</h1></div>
-              <div className='item'><h1>Patch Adams</h1></div>
-              <div className='item'><h1>Elf</h1></div>
-            </div>
+            <b /><b /><b />
             <div className="container">
-              <div className='item'><h1>Shawshank Redemption</h1></div>
-              <div className='item'><h1>St Elmos Fire</h1></div>
-              <div className='item'><h1>Breakfast Club</h1></div>
-            </div>
-          </div>
+              <div className='item'>
+                <img src={Mulan} alt='Mulan logo' />
+               </div>
+               <div className='item'>
+                  <img src={PatchAdams} alt='Patch Adams logo' />
+               </div>
+                  <div className='item'>
+                      <img src={Elf} alt='Elf logo' />
+                   </div>
+                  </div>
+                  <div className="container">
+                    <div className='item'>
+                      <img src={ShawshankRedemption} alt='Shawshank Redemption logo' />
+                    </div>
+                    <div className='item'>
+                      <img src={StElmos} alt='St Elmos Fire logo' />
+                    </div>
+                    <div className='item'>
+                      <img src={BfastClub} alt='Breakfast Club logo' />
+                  </div>
+              </div>
+          </div >
       );
   }
 }
 
 export default App;
-/*h1 style = "font-size: 60px;"> Heading 1</h1></div>*/
