0752ebdd095179655942f6028770d600bd3ba5eb
S B <sb@DESKTOP-771PQMS.localdomain>
2019-04-11 16:56:12 +0100

    Introduce component state_MovGall_Details_13

---
+++ src/StatefulMsg_Details_13.js
@@ -0,0 +1,31 @@
+// 13 Introduce component state
+//Stateless Fn to stateful component
+
+import React, {component} from 'react';
+import { Link } from 'react-router-dom';
+
+export default class Message extends component() {
+  constructor() {
+      super();
+      this.state = {
+        welcomeMessage: 'Hello, this will be the details page for each Movie & TV show :)'  //display welcome msg
+      };
+  }
+  
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState ({
+          welcomeMessage: 'Coming soon! :)'  //display updated msg
+      });
+  }, 3000);
+}
+  
+  render () {
+    return (
+      <div>
+          <h1>this.state.welcomeMessage</h1>
+          <Link to='/'>Back to home page</Link>
+      </div>
+    );
+  }
+}
