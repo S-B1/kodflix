5c7ffd9cf057a592b81f189d0fcf50b526676399
S-B1 <sharbraham@icloud.com>
2019-04-20 09:10:59 +0100

    Move the covers data model to a proper frontend data_12

--- src/MovGall_get.js
+++ src/MovGall_get.js
@@ -1,9 +1,3 @@
-// 12 Move the covers data model to a proper front-end data
-//UI improved and can easily be changed in the future
-//New arr utility component
-
-//Don't need to import React
-//Ensure the logo and import name match
 
 import Mulan from './Images/Mulan.jpeg';
 import PatchAdams from './Images/PatchAdams.jpeg';
@@ -11,12 +5,6 @@ import Elf from './Images/Elf.jpeg';
 import ShawshankRedemption from './Images/Shawshank.jpeg';
 import StElmos from './Images/StElmos.jpeg';
 import BfastClub from './Images/BfastClub.jpeg';
-
-//Create a Fn to get the MovieGallery
-//replicate data model with pure JS arr. Bce easy to edit.
-//From static to dynamic list of items
-//Typical for react, a list of items from arr using .map to loop over arr. 
-//Each indiv elem
 
 export default function getMovieGallery() {
   return [
--- src/MovieGallery.js
+++ src/MovieGallery.js
@@ -1,8 +1,3 @@
-// 12 Move the covers data model to a proper front-end data
-// To maintain data for UI, using data model using arr of items to layout component. 
-// It will iterate on list of components.
-// Rtn list 
-// In JS easy to work in arr
 
 import React from 'react';
 import Stack from './Stack';
@@ -19,8 +14,6 @@ export default function MovieGallery() {
             id={stack.id} 
             name={stack.name} 
             logo={stack.logo} />
-            //could use <div></div>
-            //render stack component. Ref stack obj, can be replaced with id, name and logo.
           ))
         }
       </div>
