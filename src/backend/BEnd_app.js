a4dec40b69547b7a05b0dddc5b5cd13bb74985bb
S-B1 <sharbraham@icloud.com>
2019-05-10 15:27:01 +0100

    Update name to BEnd_app.js. Change require to MovList_20

--- src/backend/BEnd_app.js
+++ src/backend/BEnd_app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
-const port = 3000;
-const getShows = require('./shows')
+const port = 3001;
+const getShows = require('./MovList')
 
 app.get('/rest/shows', (req, res) => res.send(getShows()));
 
